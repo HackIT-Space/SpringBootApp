5fac5379c582e60c80156071ce82d474807d3ffe
Vasily Nikitin <vasyan.coder@gmail.com>
2025-05-18 14:38:20 +0300

    initialize project structure with Spring Boot, PostgreSQL, and CI configuration

---
+++ src/main/java/org/hackit/HackitApplication.java
@@ -0,0 +1,13 @@
+package org.hackit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hackit/HackitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackitApplicationTests {
+
+    @Test
+    void simpleTest() {
+    }
+
+}
